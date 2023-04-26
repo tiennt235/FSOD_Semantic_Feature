2e1f25579c1aedf1bef76dd53a1f535cbf6535e2
tiennt235 <tiennt235@gmail.com>
2023-04-26 14:39:39 +0700

    distillate multi scale features

--- run_voc_distillate_rpn.sh
+++ run_voc_distillate_rpn.sh
@@ -1,4 +1,4 @@
-EXP_NAME="distillate_rpn_res234_expand"
+EXP_NAME="distillate_rpn_res234_expand_defrost"
 SPLIT_ID=1
 SAVE_DIR=checkpoints/voc/${EXP_NAME}
 IMAGENET_PRETRAIN=ImageNetPretrained/MSRA/R-101.pkl
@@ -7,12 +7,20 @@ IMAGENET_PRETRAIN_TORCH=ImageNetPretrained/torchvision/resnet101-5d3b4d8f.pth
 NUM_GPUS=4
 export CUDA_VISIBLE_DEVICES=4,5,6,7
 
-
+# res4 only
 cfg_MODEL="
    MODEL.META_ARCHITECTURE GeneralizedDistillatedRCNN
-   MODEL.RESNETS.OUT_FEATURES ['res2','res3','res4'] 
    MODEL.ADDITION.NAME glove
 "
+
+# multi-scale resnets features
+cfg_MODEL="
+   MODEL.META_ARCHITECTURE GeneralizedDistillatedRCNN
+   MODEL.BACKBONE.FREEZE_AT 1
+   MODEL.RESNETS.OUT_FEATURES ['res2','res3','res4']
+   MODEL.ADDITION.NAME glove
+"
+
 
 python3 main.py --num-gpus ${NUM_GPUS} --config-file configs/voc/defrcn_det_r101_base${SPLIT_ID}.yaml \
    --opts MODEL.WEIGHTS ${IMAGENET_PRETRAIN} \
